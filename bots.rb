8778523d36ac8a96e034c129c7d88e668d2213aa
Karl Parakenings <karl.parakenings@payfirma.com>
2014-06-18 13:23:32 -0700

    full archive

--- bots.rb
+++ bots.rb
@@ -15,7 +15,7 @@ Ebooks::Bot.new("karlnp_ebooks") do |bot|
 
   bot.on_startup do 
     for i in 0..3
-      model = Ebooks::Model.load("model/karlnp.model")
+      model = Ebooks::Model.load("model/tweets.model")
       #model = Ebooks::Model.load("model/combined.model")
       bot.tweet(model.make_statement(140))
     end
